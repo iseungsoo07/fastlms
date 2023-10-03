6285b39df73d98cddfb2ac0b9eb840a0b4b32900
iseungsoo07 <iseungsoo07@gmail.com>
2023-10-03 13:30:10 +0900

    이미지 엑스박스 나오는거 해결

---
+++ src/main/java/com/zerobase/fastlms/configuration/WebConfig.java
@@ -0,0 +1,15 @@
+package com.zerobase.fastlms.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/files/**")
+                .addResourceLocations("file:/Users/iseungsoo07/zerobase/fastlms3/src/main/resources/static/files/");
+    }
+}
